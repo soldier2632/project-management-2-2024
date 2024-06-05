dd0a51956a6fe787d1c2e9bc42e66e0c05e2f7cb
soldier2632 <fckutony173@gmail.com>
2024-06-05 22:38:23 +0700

    lấy data sản phẩm in ra giao

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,7 +1,12 @@
+const Product = require('../../models/product.model');
 //[GET] /admin/product
-module.exports.index = (req,res)=>{
+module.exports.index = async (req,res)=>{
+    const products = await Product.find({
+        deleted : false,
+    });
     res.render("admin/pages/products/index.pug",{
         pagetitle:"trang san pham",
+        products : products
     });
 
 }
