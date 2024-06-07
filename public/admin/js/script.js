4af1473fd49fd9737ecf51940bc462ef7fc7da0f
soldier2632 <fckutony173@gmail.com>
2024-06-07 22:04:21 +0700

    tính năng tìm kiếm( Frond-end)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,19 +1,36 @@
-const buttonsStatus = document.querySelectorAll('[button-status]');
-if(buttonsStatus.length > 0){
-    let url = new URL(window.location.href);
-    console.log(url);
-    buttonsStatus.forEach(button => {
-        button.addEventListener('click',()=>{
-            const status = button.getAttribute("button-status");
-            
-        if(status){
-            url.searchParams.set("status", status)
-        }else{
-            url.searchParams.delete("status")
-        }
-        // console.log(url.href);
-        window.location.href = url.href;
-        });
+// button status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+  // console.log(url);
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      // console.log(url.href);
+      window.location.href = url.href;
     });
+  });
 }
-// console.log("hello world")
+//End button status
+// Form search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+    const url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
+// End Form search
