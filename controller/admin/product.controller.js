2d8f341727543ff00b3ba8a8c1ad783e0657b346
soldier2632 <fckutony173@gmail.com>
2024-06-07 21:01:21 +0700

    tính năng lọc sản phẩm theo trạng thái

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,12 +1,44 @@
-const Product = require('../../models/product.model');
+const Product = require("../../models/product.model");
 //[GET] /admin/product
-module.exports.index = async (req,res)=>{
-    const products = await Product.find({
-        deleted : false,
-    });
-    res.render("admin/pages/products/index.pug",{
-        pagetitle:"trang san pham",
-        products : products
-    });
+module.exports.index = async (req, res) => {
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status === req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status === "");
+    filterStatus[index].class = "";
+  }
+  let find = {
+    deleted: false,
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
 
-}
+  const products = await Product.find(find);
+  // console.log(products);
+  res.render("admin/pages/products/index.pug", {
+    pagetitle: "trang san pham",
+    products: products,
+    filterStatus: filterStatus,
+  });
+};
---
+++ public/admin/js/script.js
@@ -0,0 +1,19 @@
+const buttonsStatus = document.querySelectorAll('[button-status]');
+if(buttonsStatus.length > 0){
+    let url = new URL(window.location.href);
+    console.log(url);
+    buttonsStatus.forEach(button => {
+        button.addEventListener('click',()=>{
+            const status = button.getAttribute("button-status");
+            
+        if(status){
+            url.searchParams.set("status", status)
+        }else{
+            url.searchParams.delete("status")
+        }
+        // console.log(url.href);
+        window.location.href = url.href;
+        });
+    });
+}
+// console.log("hello world")
