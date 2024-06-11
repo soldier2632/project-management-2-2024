097d7c2325b10f6ccaf9465ae17dd99fd7cdbcde
soldier2632 <fckutony173@gmail.com>
2024-06-11 17:08:06 +0700

    tối ưu hóa tìm kiếm

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,21 +1,18 @@
 const Product = require("../../models/product.model");
-const filterStatusHelper = require("../../helper/filterStatus");
+const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 //[GET] /admin/product
 module.exports.index = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
- 
-   let keyword = "";
-  
+  const objectSearch = searchHelper(req.query);
   let find = {
     deleted: false,
   };
   if (req.query.status) {
     find.status = req.query.status;
   }
-   if(req.query.keyword){
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword, "i");
-    find.title = regex;
+    if(objectSearch.regex){
+     find.title= objectSearch.regex;
    }
   const products = await Product.find(find);
   // console.log(products);
@@ -23,6 +20,6 @@ module.exports.index = async (req, res) => {
     pagetitle: "trang san pham",
     products: products,
     filterStatus: filterStatus,
-    keyword:keyword
+    keyword:objectSearch.keyword,
   });
 }
