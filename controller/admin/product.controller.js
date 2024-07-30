eaa7ec61c959b471db9f6978d73a3fc286205c45
soldier2632 <fckutony173@gmail.com>
2024-07-30 10:06:30 +0700

    tính năng tạo mới sản phẩm

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+const systemConfig = require("../../config/system");
 //[GET] /admin/product
 module.exports.index = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
@@ -12,85 +13,111 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
-  if(objectSearch.regex){
-     find.title= objectSearch.regex;
-  } 
-   //pagination
-   const countProducts = await Product.countDocuments(find);
-  let objectPagination= paginationHelper({
-      currentPage :1,
-      limitItems :4
-  },
-   req.query,
-   countProducts  
-)
-//end pagination
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+  //pagination
+  const countProducts = await Product.countDocuments(find);
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 4,
+    },
+    req.query,
+    countProducts
+  );
+  //end pagination
   const products = await Product.find(find)
-  .limit(objectPagination.limitItems)
-  .skip(objectPagination.skip)
-  .sort({position: "desc"});
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip)
+    .sort({ position: "desc" });
 
   res.render("admin/pages/products/index.pug", {
     pagetitle: "trang san pham",
     products: products,
     filterStatus: filterStatus,
-    keyword:objectSearch.keyword,
-    pagination: objectPagination
+    keyword: objectSearch.keyword,
+    pagination: objectPagination,
   });
-}
+};
 //[PATCH] /admin/product/change-status/:status/:id
-module.exports.changeStatus= async (req,res)=>{
+module.exports.changeStatus = async (req, res) => {
   const status = req.params.status;
   const id = req.params.id;
-    await Product.updateOne({_id:id},{status:status});
-    req.flash('success', 'Cập nhật trạng thái thành công!');
-    res.redirect("back");   
-}
+  await Product.updateOne({ _id: id }, { status: status });
+  req.flash("success", "Cập nhật trạng thái thành công!");
+  res.redirect("back");
+};
 //[PATCH] /admin/product/change-multi
-module.exports.changeMulti= async (req,res)=>{
-   const type = req.body.type;
-   const ids = req.body.ids.split(", ");
-console.log(type);
-console.log(ids);
-   switch(type){
+module.exports.changeMulti = async (req, res) => {
+  const type = req.body.type;
+  const ids = req.body.ids.split(", ");
+  console.log(type);
+  console.log(ids);
+  switch (type) {
     case "active":
-      await Product.updateMany({_id:{$in: ids}}, {status: "active"});
-      req.flash('success', `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+      await Product.updateMany({ _id: { $in: ids } }, { status: "active" });
+      req.flash(
+        "success",
+        `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`
+      );
       break;
     case "inactive":
-      await Product.updateMany({_id:{$in: ids}}, {status: "inactive"});
-      req.flash('success', `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+      await Product.updateMany({ _id: { $in: ids } }, { status: "inactive" });
+      req.flash(
+        "success",
+        `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`
+      );
       break;
     case "delete-all":
-    await Product.updateMany({_id:{$in: ids}}, {deleted:true, deleteAt: new Date()});
-    break;
+      await Product.updateMany(
+        { _id: { $in: ids } },
+        { deleted: true, deleteAt: new Date() }
+      );
+      break;
     case "change-position":
       for (const item of ids) {
-        let [id, position] =item.split("-");
-       
+        let [id, position] = item.split("-");
+
         position = parseInt(position);
-         await Product.updateOne(
-           {_id: id},
-           {position : position});
+        await Product.updateOne({ _id: id }, { position: position });
       }
       break;
     default:
-      break;  
-   }
+      break;
+  }
   res.redirect("back");
   // res.send("ok")
-}
+};
 //[DELETE] /admin/product/delete/:id
-module.exports.deleteItem= async (req,res)=>{
+module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-    // await Product.deleteOne({_id:id});
-    await Product.updateOne(
-      {_id:id},
-      {deleted:true,
-       deletedAt: new Date()
-      }
-      );
-    res.redirect("back");   
-}
-  
+  // await Product.deleteOne({_id:id});
+  await Product.updateOne(
+    { _id: id },
+    { deleted: true, deletedAt: new Date() }
+  );
+  res.redirect("back");
+};
+//[GET] /admin/products/create
+module.exports.createItem = async (req, res) => {
+  res.render("admin/pages/products/create.pug", {
+    pagetitle: "thêm mới sản phẩm",
+  });
+};
+//[POST] /admin/products/create
+module.exports.createPost = async (req, res) => {
+  req.body.price = parseInt(req.body.price);
+  req.body.discountPercentage = parseInt(  req.body.discountPercentage);
+  req.body.stock = parseInt(req.body.stock);
+  if(req.body.position == ""){
+    const countProducts = await Product.countDocuments();
+    req.body.position = countProducts + 1;
+  }else{
+    req.body.position = parseInt(req.body.position);
+  }
+  const product = new Product(req.body);
+  await product.save();
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+};
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -2,9 +2,15 @@ const express = require('express');
 const productController = require('../../controller/admin/product.controller');
 
 const routes = express.Router();
-
+//route.get
 routes.get('/', productController.index);
+//route.patch
 routes.post('/change-status/:status/:id', productController.changeStatus);
 routes.post('/change-multi', productController.changeMulti);
+//route.delete
 routes.post('/delete/:id', productController.deleteItem);
+//route.get
+routes.get('/create', productController.createItem);
+//route.post
+routes.post('/create',  productController.createPost);
 module.exports = routes;
